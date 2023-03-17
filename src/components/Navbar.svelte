4cbf3d10895692cbd1e81b1885bc2fca8b0b2909
149segolte <37300847+149segolte@users.noreply.github.com>
2023-03-17 17:49:08 +0530

    Recover

---
+++ src/components/Navbar.svelte
@@ -0,0 +1,19 @@
+<nav class="py-8 px-12 flex">
+    <div class="brand"><a href="/">149segolte</a></div>
+    <ul class="flex text-white items-center ml-auto mr-8 gap-4">
+        <li class=""><a href="/#about">About</a></li>
+        <li class=""><a href="/#Experience">Experience</a></li>
+        <li class=""><a href="/#Work">Work</a></li>
+        <li class=""><a href="/#Contact">Contact</a></li>
+    </ul>
+</nav>
+
+<style>
+    .brand {
+        @apply font-['Bebas_Neue'] m-0 h-full text-6xl text-white;
+    }
+
+    nav > ul {
+        @apply text-xl font-sans;
+    }
+</style>
