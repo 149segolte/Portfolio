8f01f47a2c8d9c648c47d26c9511dfbf11b15c00
149segolte <37300847+149segolte@users.noreply.github.com>
2023-06-01 14:48:20 +0530

    Update the landing page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
+  integrations: [tailwind(), svelte()],
+  experimental: {
+    assets: true
+  }
 });
